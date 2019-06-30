b60abf1d2044469221c24ab022bd89778afdccab
sonicaks <adityakumarsinha2014@gmail.com>
2019-06-30 16:55:11 +0530

    Fix errors

--- social_media/accounts/models.py
+++ social_media/accounts/models.py
@@ -4,5 +4,5 @@ from django.contrib import auth
 # Create your models here.
 
 class User(auth.models.User, auth.models.PermissionsMixin):
-    def __str__self):
+    def __str__(self):
         return "@{}".format(self.username)
--- social_media/accounts/urls.py
+++ social_media/accounts/urls.py
@@ -6,6 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView(), name = 'logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('signup/', views.SignUp.as_view(), name = 'signup')
 ]
